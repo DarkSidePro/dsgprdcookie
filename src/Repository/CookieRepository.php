813b4a625c95c90e6406ba8dbbdd7dce1ca2ff1e
Patryk Tchorzewski <patryk@dark-side.pro>
2023-12-20 18:03:56 +0100

    init

---
+++ src/Repository/CookieRepository.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0).
+ * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
+ */
+declare(strict_types=1);
+
+namespace DarkSide\DsGPRDCookie\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+
+class CookieRepository extends EntityRepository
+{
+
+}
