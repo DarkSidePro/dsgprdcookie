182d5c20ff8abb0bffbfedbc9689aa43b79eee7f
Patryk Tchorzewski <patryk@dark-side.pro>
2024-01-02 18:05:17 +0000

    init findFieldLangByIdLang

--- src/Repository/CookieFieldLangRepository.php
+++ src/Repository/CookieFieldLangRepository.php
@@ -11,10 +11,41 @@ declare(strict_types=1);
 
 namespace DarkSide\DsGprdCookie\Repository;
 
+use DarkSide\DsGprdCookie\Entity\DsGprdCookieField;
+use DarkSide\DsGprdCookie\Entity\DsGprdCookieFieldLang;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
 
 class CookieFieldLangRepository extends EntityRepository
 {
+    /**
+     * @param int $lang_id
+     * @see array_reduce() 
+     * 
+     * @return array
+     */
+    public function findFieldLangByIdLang(int $lang_id): array
+    {
+        $fields = $this->createQueryBuilder('f')
+            ->select('cfl.field_name', 'cfl.text_value')
+            ->from(DsGprdCookieFieldLang::class, 'cfl')
+            ->leftJoin(DsGprdCookieField::class, 'cf', Join::WITH, 'cfl.field = cf.id')
+            ->andWhere('cfl.id_lang = :id_lang')
+            ->setParameter(':id_lang', $lang_id)
+            ->getQuery()
+            ->getResult(Query::HYDRATE_ARRAY);
 
+        $transformedArray = array_reduce($fields, function ($result, $element) {
+            $fieldName = $element['field_name'];
+        
+            // Dodaj nowy element do wynikowej tablicy
+            $result[$fieldName] = $element;
+        
+            return $result;
+        }, []);
+        
+        return $transformedArray;
+    }
 }
